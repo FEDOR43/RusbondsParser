d9e5b436b7f30c4c4208b8b007bc24ec756aa38f
Fedorov Anton <afedorov43@gmail.com>
2022-06-13 11:51:53 +0300

    Installed BS4.

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from bs4 import BeautifulSoup
+import requests
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def get_info():
+    url = 'https://old.rusbonds.ru/compare.asp'
+    bond = 'BCS Structured-33-2025-ев'
+    # page = requests.get(f'{url}?go=1&tool={bond}')
+    page = requests.get(url)
+
+    soup = BeautifulSoup(page.content, "html.parser")
+    print(soup.get_text())
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    get_info()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
