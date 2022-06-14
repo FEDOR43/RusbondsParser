cf2aaa6757a6a38e252f0a75c2a4de03be067a44
Fedorov Anton <afedorov43@gmail.com>
2022-06-14 05:43:00 +0300

    Temp commit.

--- main.py
+++ main.py
@@ -7,9 +7,33 @@ def get_info():
     bond = 'BCS Structured-33-2025-ев'
     # page = requests.get(f'{url}?go=1&tool={bond}')
     page = requests.get(url)
+    page.encoding = 'windows-1251'
 
-    soup = BeautifulSoup(page.content, "html.parser")
-    print(soup.get_text())
+    soup = BeautifulSoup(page.text, "lxml")
+
+    # tables = soup.find('table', {'class': 'tbl_data.tbl_headgrid'})
+    tables = soup.find(class_='tbl_data tbl_headgrid')
+    result = []
+    for table in tables:
+        result += [{table.text: table.next_element.text}]
+    print(result)
+
+    # tables = soup.findAll('tbody')
+    # for table in tables:
+    #     print(table.fetchParents)
+    #     print()
+
+
+    # links = (soup.findAll(class_='bl'))
+    # for link in links:
+    #     l = link.get('href')
+    #     print(f'{link.text}: {l}')
+    # field = soup.find('input', {'class': 'gsm', 'onchange': 'document.frm1.emit.selectedIndex=0'})
+    # btn = soup.find('input', {'class': 'btc', 'value': '   найти   '})
+    # print(field)
+    # print(btn)
+    # soup2 = BeautifulSoup(field.get(bond), 'lxml')
+    # print(soup2)
 
 
 # Press the green button in the gutter to run the script.
