bbe9abcf0c2200842a30f43e08f5ec55db8f8063
DESKTOP-2MQQLTS\fedor <afedorov43@gmail.com>
2022-06-18 18:09:37 +0300

    Dictionary result.

--- main.py
+++ main.py
@@ -6,7 +6,26 @@ def get_info():
     site = 'https://old.rusbonds.ru'
     url_bonds = '/compare.asp'
     url = f'{site}{url_bonds}'
-    column_names = [
+    column_names_eng = [
+        'Bond',
+        'Payment date',
+        'Offer date',
+        'Macaulay duration, days:',
+        'Coupon rub/year',
+        'Coupon % per year',
+        'Coupon NKD',
+        'Price, % nominal clean',
+        'Price, % nominal rev.',
+        'Price, % nominal dirty',
+        'Effective yield, % annual repayment',
+        'Effective yield, % annual change',
+        'Effective yield, % annual offer',
+        'Trades for the week, trades',
+        'Weekly trading volume, millions',
+        'In circulation, millions',
+        'Turnover ratio x 100%',
+    ]
+    column_names_rus = [
         'Облигация, выпуск',
         'Дата погаш.',
         'Дата оферты.',
@@ -25,32 +44,42 @@ def get_info():
         'В обращении, млн.',
         'Коэф. оборач. х 100%'
     ]
-    # bond = 'BCS Structured-33-2025-ев'
+
     page = requests.get(url)
     page.encoding = 'windows-1251'
 
     soup = BeautifulSoup(page.text, "lxml")
 
-    tables = soup.find(class_='tbl_data tbl_headgrid')
-    result = []
-    result2 = []
-    for table in tables:
-        lines = table.find_all('tr')
+    table = soup.find(class_='tbl_data tbl_headgrid')
+    result_tmp = []
+    result_list = []
+    for element in table:
+        lines = element.find_all('tr')
         for line in lines:
             columns = line.find_all('td')
             for column in columns:
                 s = line.text
-                if not s.startswith('Облигация') and not s.startswith('р/год') and not s.startswith('* - сделки сегодня'):
-                    result.append(column.text.replace('\xa0', '').strip())
-                if result != []:
-                    result2.append(result)
-            result = []
+                if not s.startswith('Облигация')\
+                        and not s.startswith('р/год')\
+                        and not s.startswith('* - сделки сегодня'):
+                    result_tmp.append(column.text.replace('\xa0', '').strip())
+                if result_tmp != []:
+                    result_list.append(result_tmp)
+            result_tmp = []
 
+    all_bonds = {}
+    for bond in result_list:
+        tmp = {}
+        for i in range(len(bond)):
+            entry = {
+                column_names_eng[i]: bond[i]
+            }
+            tmp.update(entry)
+        obl = {bond[0]: tmp}
+        all_bonds.update(obl)
 
-
-    print(result2)
-
-
+    print(all_bonds)
+    print(all_bonds.get('BCS Structured-10-2022-ев').get('Payment date'))
 
     # links = (soup.findAll(class_='bl'))
     # for link in links:
